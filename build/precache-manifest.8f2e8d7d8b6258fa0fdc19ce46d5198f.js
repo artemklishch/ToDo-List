dea6f1fb5733aff83b371057b12b258605e911a4
Artem <artemklishch@gmail.com>
2020-02-24 18:11:56 +0200

    build

---
+++ build/precache-manifest.8f2e8d7d8b6258fa0fdc19ce46d5198f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49bab8b31621be19e74d8b47c8c372e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3526519e36b977dd805",
+    "url": "/static/css/main.0637a388.chunk.css"
+  },
+  {
+    "revision": "9f5614ac20966d5a0400",
+    "url": "/static/js/2.eb2dc991.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.eb2dc991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3526519e36b977dd805",
+    "url": "/static/js/main.610073e1.chunk.js"
+  },
+  {
+    "revision": "358b3f030b0ae0ca827c",
+    "url": "/static/js/runtime-main.ff225a3e.js"
+  }
+]);
