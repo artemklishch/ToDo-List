32fecb30c2d1fa0091b9de424792fb2811eee645
Artem <artemklishch@gmail.com>
2020-03-13 15:51:52 +0200

    Updates

---
+++ precache-manifest.526d70f5c4e22fbcdab6a332cb21625b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a324536e3ac62ab651182075dca4f52b",
+    "url": "/ToDo-List/index.html"
+  },
+  {
+    "revision": "005fcb5becf6aa56aaef",
+    "url": "/ToDo-List/static/css/main.09ab9eee.chunk.css"
+  },
+  {
+    "revision": "9f5614ac20966d5a0400",
+    "url": "/ToDo-List/static/js/2.eb2dc991.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/ToDo-List/static/js/2.eb2dc991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "005fcb5becf6aa56aaef",
+    "url": "/ToDo-List/static/js/main.610073e1.chunk.js"
+  },
+  {
+    "revision": "31f8877db21fc82d4406",
+    "url": "/ToDo-List/static/js/runtime-main.925880c3.js"
+  }
+]);
